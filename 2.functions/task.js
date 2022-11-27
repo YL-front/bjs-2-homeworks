995d36a65024ad950c2137c224a08ef93e1b5031
Lana <yarullina.1997@list.ru>
2022-11-28 00:57:29 +0300

    functions getArrayParams, worker, makeWork, worker2 done

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -2,16 +2,33 @@
 function getArrayParams(arr) {
   let min, max, sum, avg;
 
-  // Ваш код
+  min = Infinity; // arr[0];
+  max = -Infinity; // arr[0];
+  sum = 0;
+
+  for (let i=0; i < arr.length; i++) {
+    if(arr[i] > max) {
+      max = arr[i];
+    }
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+      
+    sum += arr[i];
+  }
+  
+  avg = Number((sum/arr.length).toFixed(2));
 
   return { min: min, max: max, avg: avg };
 }
 
 // Задание 2
 function worker(arr) {
-  let sum;
+  let sum = 0;
 
-  // Ваш код
+  for (let i=0; i < arr.length; i++) {
+    sum += arr[i];
+  }
 
   return sum;
 }
@@ -19,13 +36,30 @@ function worker(arr) {
 function makeWork(arrOfArr, func) {
   let max;
 
-  // Ваш кода
-  // for ...
+  max = 0;
+  
+  for (let i=0; i < arrOfArr.length; i++) {
+    let sum = func(arrOfArr[i]);
+    if (sum > max) {
+      max = sum;
+    }
+  }
   
   return max;
 }
 
 // Задание 3
 function worker2(arr) {
-  // Ваш код
+  let min = Infinity;
+  let max = -Infinity;
+  for (let i=0; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+  }
+  let difference = Math.abs(max - min);
+  return difference;
 }
