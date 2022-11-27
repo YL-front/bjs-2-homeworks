64fd219a6900a2fbf3b06c897ff22a2aed7f1f5c
Lana <yarullina.1997@list.ru>
2022-11-27 22:36:51 +0300

    add function solveEquation and calculateTotalMortgage

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,13 +1,59 @@
+"use strict";
+
 function solveEquation(a, b, c) {
   let arr;
-  // код для задачи №1 писать здесь
+
+  let d = b**2-4*a*c;
+
+  if (d<0) {
+    arr = [];
+  }
+  
+  if (d === 0) {
+     arr = [-b/(2*a)];
+  }
+
+  if(d>0) {
+    arr = [
+      (-b + Math.sqrt(d) )/(2*a),
+      (-b - Math.sqrt(d) )/(2*a)
+    ]; 
+  }
   return arr; // array
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
   let totalAmount;
+  let P;
+  let S;
 
-  // код для задачи №2 писать здесь
+  if(Number.isInteger(percent)) {
+     P = percent/100;
+  }  else {
+    console.log(`Параметр percent(проценнтная ставка) содержит неправильное значение ${percent}`);
+  }
+  
+  if(Number.isInteger(contribution) === false) {
+    console.log(`Параметр contribution(начальный бонус) содержит неправильное значение ${contribution}`);
+  }
 
-  return totalAmount;
+  if(Number.isInteger(amount) === false) {
+    console.log(`Параметр amount(сумма кредита) содержит неправильное значение ${amount}`);
+  }
+
+  if(Number.isInteger(date) === false) {
+    console.log(`Параметр date(срок в месяцах) содержит неправильное значение ${date}`);
+  }
+
+  
+  S = amount - contribution;
+  let dateInMonth = date.getMonth();
+  let r = dateInMonth/12;
+  let mounthPayment = S * (P + (P / (((1 + P)**dateInMonth) - 1)));
+  
+  //totalAmount = S*P/P;
+  totalAmount = amount*r/(1-(1/(1+r))*dateInMonth);
+
+  console.log(totalAmount.toFixed(2));
+  return totalAmount.toFixed(2);
 }
