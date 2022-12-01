291304dd8e4e4420e5c6a6cd54fb70b4338ec76b
Lana <yarullina.1997@list.ru>
2022-12-01 16:26:33 +0300

    added results of functions compareArrays and advancedFilter

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,7 @@
 function compareArrays(arr1, arr2) {
   let result;
 
-  // Ваш код
+  result = arr1.length === arr2.length  && arr1.every((element, index) => element === arr2[index]);
 
   return result; // boolean
 }
@@ -9,7 +9,7 @@ function compareArrays(arr1, arr2) {
 function advancedFilter(arr) {
   let resultArr;
 
-  // Ваш код
+  resultArr = arr.filter( e => e > 0).filter(e => e % 3 === 0).map(e => e*10);
 
   return resultArr; // array
 }
