be58bf895bf9344e8f0394690da02bd538ec5bc4
Lana <yarullina.1997@list.ru>
2022-12-12 20:11:18 +0300

    task4 done

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,75 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
 }
+
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+  this.subject = subjectName;
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function (mark) {
+  if (this.marks === undefined) {
+    this.marks = [mark];
+  } else {
+    this.marks.push(mark);
+  }
+}
+
+ Student.prototype.addMarks = function (...marks) {
+  if (this.marks === undefined) {
+    this.marks = [...marks];
+  } else {
+    this.marks.push(...marks);
+  }
+}
+
+
+
+Student.prototype.getAverage = function () {
+  let sum = 0;
+  let avg = 0;
+  for (i=0; i<this.marks.length; i++) {
+    sum += this.marks[i];
+  }
+  return avg = sum/this.marks.length;
+}
+
+Student.prototype.exclude = function (reason) {
+  delete this.subject;
+  delete this.marks;
+  this.exclude = reason;
+}
+
+let student1 = new Student("Jane", "female", 16);
+student1.setSubject("Algebra");
+student1.addMark(5);
+student1.addMark(4);
+student1.addMark(5);
+console.log(student1.getAverage()); 
+console.log(student1);
+
+let student2 = new Student("John", "male", 17);
+student1.setSubject("Informatics");
+student1.addMark(4);
+student1.addMark(4);
+student1.addMark(5);
+console.log(student2.getAverage());  
+console.log(student2);
+
+let student3 = new Student("Sammer", "female", 17);
+student1.setSubject("Literature");
+student1.addMark(5);
+student1.addMark(3);
+student1.addMark(4);
+console.log(student3.getAverage()); 
+console.log(student3);
+
+let student4 = new Student("Morty", "male", 15);
+student1.setSubject("Geometry");
+student1.addMark(3);
+student1.addMark(3);
+student1.addMark(5);
+console.log(student4.getAverage()); 
+console.log(student4);
